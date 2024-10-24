8a4f9d36d179ef7a01d389d25ebdec54357e1a30
sg <spyros@ocurity.com>
2024-10-24 16:20:57 +0100

    address #332, make the pdf consumer play well with the rest of the build system

--- components/consumers/pdf/main.go
+++ components/consumers/pdf/main.go
@@ -8,20 +8,17 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"html/template"
 	"log"
+	"log/slog"
 	"os"
 	"path/filepath"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/playwright-community/playwright-go"
-
 	"github.com/smithy-security/smithy/components/consumers"
+	playwright "github.com/smithy-security/smithy/pkg/playwright"
+	s3client "github.com/smithy-security/smithy/pkg/s3"
 )
 
 var (
@@ -31,6 +28,7 @@ var (
 )
 
 func main() {
+
 	flag.StringVar(&bucket, "bucket", "", "s3 bucket name")
 	flag.StringVar(&region, "region", "", "s3 bucket region")
 	flag.StringVar(&reportTemplate, "template", "", "report html template location")
@@ -38,112 +36,93 @@ func main() {
 	if err := consumers.ParseFlags(); err != nil {
 		log.Fatal(err)
 	}
+
+	if bucket == "" {
+		log.Fatal("bucket is empty, you need to provide a bucket value")
+	}
+
+	if region == "" {
+		log.Fatal("region is empty, you need to provide a region value")
+	}
+
 	var responses any
+	var scanID string
 	if consumers.Raw {
 		r, err := consumers.LoadToolResponse()
 		if err != nil {
 			log.Fatal("could not load raw results, file malformed: ", err)
 		}
 		responses = r
+		scanID = r[0].ScanInfo.ScanUuid
 	} else {
 		r, err := consumers.LoadEnrichedToolResponse()
 		if err != nil {
 			log.Fatal("could not load enriched results, file malformed: ", err)
 		}
 		responses = r
-	}
-	result, err := buildPdf(responses)
-	if err != nil {
-		log.Fatal(err)
+		scanID = r[0].OriginalResults.ScanInfo.ScanUuid
 	}
 
-	if err = sendToS3(result, bucket, region); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func sendToS3(filename, bucket, region string) error {
-	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-	if err != nil {
-		return fmt.Errorf("unable to start session with AWS API: %w", err)
-	}
-
-	// filename is statically defined above
-	//#nosec:G304
-	data, err := os.ReadFile(filename) //#nosec:G304
-	if err != nil {
-		return fmt.Errorf("could not open file: %w", err)
-	}
-
-	uploader := s3manager.NewUploader(sess)
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(filename),
-		Body:   bytes.NewReader(data),
-	})
-	if err != nil {
-		return fmt.Errorf("unable to upload %q to %q: %w", filename, bucket, err)
-	}
-
-	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
-	return nil
-}
-
-func buildPdf(data any) (string, error) {
-	tmpl := template.Must(template.ParseFiles("default.html"))
 	cleanupRun := func(msg string, cleanup func() error) {
 		if err := cleanup(); err != nil {
-			log.Printf(msg, err)
+			slog.Error(msg, "error", err)
 		}
+	}
+
+	pw, err := playwright.NewClient()
+	if err != nil {
+		log.Fatalf("could not launch playwright: %s", err)
+	}
+
+	defer cleanupRun("could not stop Playwright: %w", pw.Stop)
+
+	client, err := s3client.NewClient(region)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := run(responses, scanID, pw, client); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(responses any, s3FilenamePostfix string, pw playwright.Wrapper, s3Wrapper s3client.Wrapper) error {
+	slog.Info("reading pdf")
+	resultFilename, pdfBytes, err := buildPdf(responses, pw)
+	if err != nil {
+		return err
+	}
+
+	slog.Info("uploading pdf to s3", slog.String("filename", resultFilename), slog.String("bucket", bucket), slog.String("region", region))
+	return s3Wrapper.UpsertFile(resultFilename, bucket, s3FilenamePostfix, pdfBytes)
+}
+
+func buildPdf(data any, pw playwright.Wrapper) (string, []byte, error) {
+	tmpl, err := template.ParseFiles("default.html")
+	if err != nil {
+		return "", nil, err
 	}
 
 	currentPath, err := os.Getwd()
 	if err != nil {
-		return "", fmt.Errorf("could not get current working directory: %w", err)
+		return "", nil, fmt.Errorf("could not get current working directory: %w", err)
 	}
 
 	reportHTMLPath := filepath.Join(currentPath, "report.html")
 	//#nosec: G304
 	f, err := os.OpenFile(reportHTMLPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o600) //#nosec: G304
 	if err != nil {
-		return "", fmt.Errorf("could not open report.html: %w", err)
+		return "", nil, fmt.Errorf("could not open report.html: %w", err)
 	}
-	defer cleanupRun("could not close file: %w", f.Close)
-
 	if err = tmpl.Execute(f, data); err != nil {
-		return "", fmt.Errorf("could not apply data to template: %w", err)
-	}
-
-	pw, err := playwright.Run()
-	if err != nil {
-		return "", fmt.Errorf("could not launch playwright: %w", err)
-	}
-	defer cleanupRun("could not stop Playwrigh: %w", pw.Stop)
-
-	browser, err := pw.Chromium.Launch()
-	if err != nil {
-		return "", fmt.Errorf("could not launch Chromium: %w", err)
-	}
-	defer cleanupRun("could not close browser: %w", browser.Close)
-
-	context, err := browser.NewContext()
-	if err != nil {
-		return "", fmt.Errorf("could not create context: %w", err)
-	}
-
-	page, err := context.NewPage()
-	if err != nil {
-		return "", fmt.Errorf("could not create page: %w", err)
+		return "", nil, fmt.Errorf("could not apply data to template: %w", err)
 	}
 
 	reportPage := fmt.Sprintf("file:///%s", reportHTMLPath)
-	if _, err = page.Goto(reportPage); err != nil {
-		return "", fmt.Errorf("could not goto page %s in the browser: %w", reportPage, err)
+	pdfBytes, err := pw.GetPDFOfPage(reportPage, reportHTMLPath)
+	if err != nil {
+		return "", nil, fmt.Errorf("could not generate pdf from page %s, err: %w", reportPage, err)
+
 	}
-
-	_, err = page.PDF(playwright.PagePdfOptions{
-		Path: playwright.String(reportHTMLPath),
-	})
-
-	return reportHTMLPath, err
+	return reportHTMLPath, pdfBytes, err
 }
