624f438987a3036262c2b1b4ab834e2e7206a030
andream16 <andrea@ocurity.com>
2024-10-29 10:40:37 +0000

    Updating component interfaces and regenerating mocks.

--- sdk/component/component.go
+++ sdk/component/component.go
@@ -20,22 +20,31 @@ type (
 		Read(ctx context.Context) ([]*ocsf.VulnerabilityFinding, error)
 	}
 
-	// Storer allows storing vulnerability findings in an underlying storage.
-	Storer interface {
-		// Store stores vulnerability findings.
-		Store(ctx context.Context, findings []*ocsf.VulnerabilityFinding) error
-	}
-
 	// Updater allows updating vulnerability findings in an underlying storage.
 	Updater interface {
 		// Update updates existing vulnerability findings.
 		Update(ctx context.Context, findings []*ocsf.VulnerabilityFinding) error
 	}
 
-	// Unmarshaler allows defining behaviours to unmarshal data into vulnerability findings format.
-	Unmarshaler interface {
-		// Unmarshal unmarshals the receiver into vulnerability finding.
-		Unmarshal() (*ocsf.VulnerabilityFinding, error)
+	// Writer allows writing non-existent vulnerability findings in an underlying storage.
+	Writer interface {
+		// Write writes non-existing vulnerability findings.
+		Write(ctx context.Context, findings []*ocsf.VulnerabilityFinding) error
+	}
+
+	// Closer allows to define behaviours to close component dependencies gracefully.
+	Closer interface {
+		// Close can be implemented to gracefully close component dependencies.
+		Close(context.Context) error
+	}
+
+	// Storer allows storing vulnerability findings in an underlying storage.
+	Storer interface {
+		Closer
+		Validator
+		Reader
+		Updater
+		Writer
 	}
 )
 
@@ -47,39 +56,27 @@ type (
 		Prepare(ctx context.Context) error
 	}
 
-	// Scanner scans a target and produces vulnerability findings.
+	// Scanner reads a scan's result and produces vulnerability findings.
 	Scanner interface {
-		Storer
-
-		// Scan performs a scan on the prepared target and returns raw data.
-		Scan(ctx context.Context) ([]Unmarshaler, error)
-		// Transform transforms the raw data into vulnerability finding format.
-		Transform(ctx context.Context, payload Unmarshaler) (*ocsf.Vulnerability, error)
+		// Transform transforms the raw scan data into vulnerability finding format.
+		Transform(ctx context.Context) ([]*ocsf.VulnerabilityFinding, error)
 	}
 
 	// Filter allows filtering out vulnerability findings by some criteria.
 	Filter interface {
-		Reader
-		Updater
-
 		// Filter returns filtered findings from the supplied ones applying some criteria.
 		// It returns false if no findings have been filtered out.
-		Filter(findings []*ocsf.VulnerabilityFinding) ([]*ocsf.VulnerabilityFinding, bool, error)
+		Filter(ctx context.Context, findings []*ocsf.VulnerabilityFinding) ([]*ocsf.VulnerabilityFinding, bool, error)
 	}
 
 	// Enricher allows enriching vulnerability findings by some criteria.
 	Enricher interface {
-		Reader
-		Updater
-
 		// Annotate enriches vulnerability findings by some criteria.
 		Annotate(ctx context.Context, findings []*ocsf.VulnerabilityFinding) ([]*ocsf.VulnerabilityFinding, error)
 	}
 
 	// Reporter advertises behaviours for reporting vulnerability findings.
 	Reporter interface {
-		Reader
-
 		// Report reports vulnerability findings on a specified destination.
 		// i.e. raises them as tickets on your favourite ticketing system.
 		Report(ctx context.Context, findings []*ocsf.VulnerabilityFinding) error
