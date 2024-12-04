4cd3d2ac88f6d415fb458ae81b2cb01dd27f0a12
andream16 <andrea@ocurity.com>
2024-12-04 17:40:49 +0000

    Implementing custom-annotation enricher.

---
+++ new-components/enrichers/custom-annotation/cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/go-errors/errors"
+
+	"github.com/smithy-security/smithy/sdk/component"
+
+	"github.com/smithy-security/smithy/new-components/enrichers/custom-annotation/internal/annotation"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+
+	if err := Main(ctx); err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func Main(ctx context.Context, opts ...component.RunnerOption) error {
+	opts = append(opts, component.RunnerWithComponentName("custom-annotation"))
+
+	cfg, err := annotation.NewConf()
+	if err != nil {
+		return errors.Errorf("error reading annotation config: %w", err)
+	}
+
+	annotator, err := annotation.NewCustomAnnotator(cfg)
+	if err != nil {
+		return errors.Errorf("error creating custom annotation annotator: %w", err)
+	}
+
+	if err := component.RunEnricher(ctx, annotator, opts...); err != nil {
+		return errors.Errorf("error enriching custom annotation: %w", err)
+	}
+
+	return nil
+}
---
+++ new-components/enrichers/custom-annotation/internal/annotation/annotation.go
@@ -0,0 +1,107 @@
+package annotation
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+
+	"github.com/go-errors/errors"
+	"github.com/smithy-security/pkg/env"
+
+	"github.com/smithy-security/smithy/sdk/component"
+	ocsf "github.com/smithy-security/smithy/sdk/gen/com/github/ocsf/ocsf_schema/v1"
+)
+
+type (
+	// Conf contains the annotation configuration.
+	Conf struct {
+		AnnotationName       string
+		AnnotationValuesJSON string
+	}
+
+	customAnnotator struct {
+		conf *Conf
+	}
+)
+
+const defaultEmptyJsonObject = "{}"
+
+// NewConf initialises the enricher configuration from environment variables.
+func NewConf() (*Conf, error) {
+	var (
+		cfg = &Conf{}
+		err error
+	)
+
+	cfg.AnnotationName, err = env.GetOrDefault(
+		"CUSTOM_ANNOTATION_NAME",
+		"",
+		env.WithDefaultOnError(true),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	annotationValues, err := env.GetOrDefault(
+		"CUSTOM_ANNOTATION_VALUES",
+		"{}",
+		env.WithDefaultOnError(true),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if annotationValues != defaultEmptyJsonObject {
+		var m = make(map[string]any)
+		if err := json.Unmarshal([]byte(annotationValues), &m); err != nil {
+			return nil, errors.Errorf("invalid JSON values supplied '%s': %w", annotationValues, err)
+		}
+		cfg.AnnotationValuesJSON = annotationValues
+	}
+
+	return cfg, nil
+}
+
+// NewCustomAnnotator returns a new custom annotator.
+func NewCustomAnnotator(conf *Conf) (*customAnnotator, error) {
+	switch {
+	case conf == nil:
+		return nil, errors.New("nil annotation configuration")
+	case conf.AnnotationName == "":
+		return nil, errors.New("annotation name cannot be empty")
+	}
+	return &customAnnotator{
+		conf: conf,
+	}, nil
+}
+
+// Annotate adds annotated values to passed findings.
+func (ca *customAnnotator) Annotate(
+	ctx context.Context,
+	findings []*ocsf.VulnerabilityFinding,
+) ([]*ocsf.VulnerabilityFinding, error) {
+	var (
+		providerName = "custom-annotation-enricher"
+		logger       = component.LoggerFromContext(ctx).
+				With(slog.Int("num_findings", len(findings))).
+				With(slog.String("provider_name", providerName)).
+				With(slog.String("annotation_name", ca.conf.AnnotationName)).
+				With(slog.Any("annotation_values", ca.conf.AnnotationValuesJSON))
+	)
+
+	logger.Debug("preparing to annotate findings...")
+
+	for idx := range findings {
+		findings[idx].Enrichments = append(
+			findings[idx].Enrichments,
+			&ocsf.Enrichment{
+				Name:     ca.conf.AnnotationName,
+				Value:    ca.conf.AnnotationValuesJSON,
+				Provider: &providerName,
+			},
+		)
+	}
+
+	logger.Debug("findings annotated successfully!")
+	return findings, nil
+}
