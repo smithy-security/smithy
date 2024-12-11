1fa7c48f67615cb559e54ca66212b2cb6a1d3ddd
andream16 <andrea@ocurity.com>
2024-12-11 17:54:43 +0000

    Updating store to use new SQLite storage constructor.

--- sdk/component/store.go
+++ sdk/component/store.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/go-errors/errors"
 
-	localstore "github.com/smithy-security/smithy/sdk/component/store/local"
+	"github.com/smithy-security/smithy/sdk/component/store/local/sqlite"
 	findingsclient "github.com/smithy-security/smithy/sdk/component/store/remote/findings-client"
 	"github.com/smithy-security/smithy/sdk/component/store/remote/postgresql"
 )
@@ -13,7 +13,7 @@ import (
 func newStore(ctx context.Context, storeType StoreType) (Storer, error) {
 	switch storeType {
 	case StoreTypeSqlite:
-		return localstore.NewManager()
+		return sqlite.NewManager(ctx)
 	case StoreTypePostgresql:
 		return postgresql.NewManager(ctx)
 	case StoreTypeFindingsClient:
