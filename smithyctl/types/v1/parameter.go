711550f7df94d1ffab913a77d09f9c9d0bc983da
andream16 <andrea@ocurity.com>
2024-12-15 13:58:04 +0000

    Open sourcing V1 types.

---
+++ smithyctl/types/v1/parameter.go
@@ -0,0 +1,142 @@
+package v1
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+var (
+	// ErrMismatchedExpectedType is returned when the type field of a parameter
+	// is not the same as the actual type of the value of a parameter.
+	ErrMismatchedExpectedType = errors.New("stated parameter type different than actual value type")
+	// ErrUnknownParameterType is returned when the type of field parameter
+	// is not one of the expected values.
+	ErrUnknownParameterType = errors.New("unknown parameter type")
+)
+
+type (
+	// ParameterType represents the type of parameter that can be parsed by the
+	// system.
+	// ENUM(string, const:string, list:string)
+	ParameterType string
+
+	// Parameter is a struct whose value must be of the type as it's defined in the
+	// Type field. Due to the fact that this value is expected to be communicated
+	// to external clients via JSON, which doesn't support rich types, we need to
+	// communicate the expected value type via an enum string. Given the Golang
+	// type system, there is no way to enforce the type restrictions via an
+	// interface, so do all the type checks when marshaling/unmarshalling the JSON
+	// bytes, since this type will constantly be subject to such transformations.
+	Parameter struct {
+		// Name is the name of the parameter.
+		Name string
+		// Type is the parameter type.
+		Type ParameterType
+		// Value is the JSON encoded/decoded value of the parameter which is decoded based its Type.
+		Value any
+	}
+)
+
+// UnmarshalJSON unmarshal JSON bytes into a Parameter object.
+func (p *Parameter) UnmarshalJSON(b []byte) error {
+	partialParameter := &struct {
+		Name string
+		Type ParameterType
+	}{}
+
+	var err error
+	if err = json.Unmarshal(b, partialParameter); err != nil {
+		return err
+	}
+
+	p.Name = partialParameter.Name
+	p.Type = partialParameter.Type
+
+	switch partialParameter.Type {
+	case ParameterTypeString, ParameterTypeConststring:
+		parameterValueStrPtr := &struct{ Value *string }{}
+		if err = json.Unmarshal(b, parameterValueStrPtr); err == nil {
+			p.Value = parameterValueStrPtr.Value
+			break
+		}
+		parameterValueStr := &struct{ Value string }{}
+		err = json.Unmarshal(b, parameterValueStr)
+		p.Value = parameterValueStr.Value
+	case ParameterTypeListstring:
+		parameterValue := &struct{ Value []string }{}
+		err = json.Unmarshal(b, parameterValue)
+		p.Value = parameterValue.Value
+	default:
+		err = ErrUnknownParameterType
+	}
+	if err != nil {
+		return fmt.Errorf("parameter.Name: %s, parameter.Type: %s: %w", partialParameter.Name, partialParameter.Type, err)
+	}
+
+	return nil
+}
+
+// MarshalJSON marshals the Parameter into JSON bytes.
+func (p *Parameter) MarshalJSON() ([]byte, error) {
+	if err := p.Validate(); err != nil {
+		return nil, err
+	}
+
+	b, err := json.Marshal(struct {
+		Name  string
+		Type  ParameterType
+		Value any
+	}{
+		Name:  p.Name,
+		Type:  p.Type,
+		Value: p.Value,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not json marshal value: %w", err)
+	}
+
+	return b, nil
+}
+
+// Validate checks all the fields of the parameter to make sure that the type
+// specified matches the actual type of the value
+func (p *Parameter) Validate() error {
+	_, err := ParseParameterType(string(p.Type))
+	if err != nil {
+		return fmt.Errorf("could not parse parameter '%s' with type: '%s': %w", p.Name, p.Type, err)
+	}
+
+	if p.Value == nil {
+		return nil
+	}
+
+	var correctType bool
+	switch p.Type {
+	case ParameterTypeString, ParameterTypeConststring:
+		_, correctType = p.Value.(*string)
+		if !correctType {
+			_, correctType = p.Value.(string)
+		}
+	case ParameterTypeListstring:
+		_, correctType = p.Value.([]string)
+	default:
+		err = ErrUnknownParameterType
+	}
+
+	if !correctType {
+		err = ErrMismatchedExpectedType
+	}
+
+	if err != nil {
+		return fmt.Errorf(
+			"invalid parameter '%s' with type '%s' and value '%v': %w",
+			p.Name,
+			p.Type,
+			p.Value,
+			err,
+		)
+	}
+
+	return nil
+}
---
+++ smithyctl/types/v1/parameter_test.go
@@ -0,0 +1,171 @@
+package v1_test
+
+import (
+	"reflect"
+	"slices"
+	"testing"
+
+	v1 "github.com/smithy-security/smithyctl/types/v1"
+)
+
+func ptr[T any](v T) *T {
+	return &v
+}
+
+func TestParameter(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range []struct {
+		testCase            string
+		param               v1.Parameter
+		expectsMarshalErr   bool
+		expectsUnmarshalErr bool
+	}{
+		{
+			testCase: "it should marshal correctly a non empty pointer to a string",
+			param: v1.Parameter{
+				Name:  "non-empty-ptr-str",
+				Type:  v1.ParameterTypeString,
+				Value: ptr("smithy"),
+			},
+		},
+		{
+			testCase: "it should marshal correctly an empty pointer to a string",
+			param: v1.Parameter{
+				Name:  "empty-ptr-str",
+				Type:  v1.ParameterTypeString,
+				Value: "",
+			},
+		},
+		{
+			testCase: "it should marshal correctly a non empty string",
+			param: v1.Parameter{
+				Name:  "non-empty-str",
+				Type:  v1.ParameterTypeString,
+				Value: "smithy",
+			},
+		},
+		{
+			testCase: "it should marshal correctly an empty string",
+			param: v1.Parameter{
+				Name:  "empty-str",
+				Type:  v1.ParameterTypeString,
+				Value: ptr(""),
+			},
+		},
+		{
+			testCase: "it should marshal correctly an nil string",
+			param: v1.Parameter{
+				Name: "nil-str",
+				Type: v1.ParameterTypeString,
+			},
+		},
+		{
+			testCase: "it should marshal correctly a non empty const pointer to a string",
+			param: v1.Parameter{
+				Name:  "non-empty-const-ptr-str",
+				Type:  v1.ParameterTypeConststring,
+				Value: ptr("{{.Helm.template.value}}"),
+			},
+		},
+		{
+			testCase: "it should marshal correctly an empty const pointer to a string",
+			param: v1.Parameter{
+				Name:  "empty-const-ptr-str",
+				Type:  v1.ParameterTypeConststring,
+				Value: ptr(""),
+			},
+		},
+		{
+			testCase: "it should marshal correctly a non empty const string",
+			param: v1.Parameter{
+				Name:  "non-empty-const-ptr-str",
+				Type:  v1.ParameterTypeConststring,
+				Value: "{{.Helm.template.value}}",
+			},
+		},
+		{
+			testCase: "it should marshal correctly an empty string",
+			param: v1.Parameter{
+				Name:  "empty-const-ptr-str",
+				Type:  v1.ParameterTypeConststring,
+				Value: "",
+			},
+		},
+		{
+			testCase: "it should marshal correctly an nil const string",
+			param: v1.Parameter{
+				Name: "nil-const-str",
+				Type: v1.ParameterTypeConststring,
+			},
+		},
+		{
+			testCase: "it should marshal correctly a non empty list string",
+			param: v1.Parameter{
+				Name:  "non-empty-list-str",
+				Type:  v1.ParameterTypeListstring,
+				Value: []string{"dracon", "is", "not", "smithy"},
+			},
+		},
+		{
+			testCase: "it should marshal correctly an empty list string",
+			param: v1.Parameter{
+				Name:  "empty-list-str",
+				Type:  v1.ParameterTypeListstring,
+				Value: make([]string, 0),
+			},
+		},
+		{
+			testCase: "it should marshal correctly an nil list string",
+			param: v1.Parameter{
+				Name: "nil-list-str",
+				Type: v1.ParameterTypeListstring,
+			},
+		},
+	} {
+		t.Run(tt.testCase, func(t *testing.T) {
+			bb, err := tt.param.MarshalJSON()
+			if tt.expectsMarshalErr && err == nil {
+				t.Fatalf("expected marshal error but got nil")
+			} else if !tt.expectsMarshalErr && err != nil {
+				t.Fatalf("expected no marshal error but got %v", err)
+			}
+
+			var param v1.Parameter
+			err = param.UnmarshalJSON(bb)
+			if tt.expectsUnmarshalErr && err == nil {
+				t.Fatalf("expected unmarshal error but got nil")
+			} else if !tt.expectsUnmarshalErr && err != nil {
+				t.Fatalf("expected no unmarshal error but got %v", err)
+			}
+
+			if tt.param.Type != v1.ParameterTypeListstring {
+				switch {
+				case tt.param.Type != param.Type:
+					t.Fatalf("expected param type %v but got %v", tt.param.Type, param.Type)
+				case tt.param.Name != param.Name:
+					t.Fatalf("expected param name %v but got %v", tt.param.Name, param.Name)
+				default:
+					// A string can be a pointer.
+					if reflect.TypeOf(param.Value).Kind() == reflect.Ptr {
+						if tt.param.Value != nil && param.Value != nil && *tt.param.Value.(*string) != *param.Value.(*string) {
+							t.Fatalf("expected param value %v but got %v", tt.param.Value, param.Value)
+						}
+						// Or not.
+					} else {
+						if tt.param.Value != param.Value {
+							t.Fatalf("expected param value %v but got %v", tt.param.Value, param.Value)
+						}
+					}
+				}
+				// In case of lists, we need to check element by element.
+			} else if tt.param.Type == v1.ParameterTypeListstring && tt.param.Value != nil {
+				for _, str := range param.Value.([]string) {
+					if !slices.Contains(tt.param.Value.([]string), str) {
+						t.Fatalf("expected list element '%s' to be found in slice '%v'", str, tt.param.Value)
+					}
+				}
+			}
+		})
+	}
+}
