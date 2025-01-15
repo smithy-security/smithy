851ec4499abf4238bcc932de2c5281e7ce5e8b38
andream16 <andrea@smithy.security>
2025-01-15 13:17:53 +0000

    Exporting V1 types as well as fixing small bug on conversion of string pointers.

--- smithyctl/tools.go
+++ smithyctl/tools.go
@@ -1,10 +1,10 @@
 //go:build tools
 
-package smithyctl
+package smithy
 
 import (
 	_ "github.com/abice/go-enum"
 )
 
 // ENUMs GEN
-//go:generate go run github.com/abice/go-enum --file ./types/v1/parameter.go
+//go:generate go run github.com/abice/go-enum --file ./pkg/types/v1/parameter.go
