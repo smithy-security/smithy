ed604eeaf47f2d9a6d19faf0ab5220ab72ea7be9
andream16 <andrea@smithy.security>
2025-02-10 17:21:01 +0000

    Implementing engine and docker container executor

---
+++ smithyctl/internal/engine/engine_test.go
@@ -0,0 +1,321 @@
+package engine_test
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+	"time"
+
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	v1 "github.com/smithy-security/smithy/pkg/types/v1"
+	"github.com/smithy-security/smithy/sdk/component/uuid"
+
+	"github.com/smithy-security/smithy/smithyctl/internal/engine"
+)
+
+func TestNewExecutor(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	t.Run("it returns an error when the container executor is nil", func(t *testing.T) {
+		exe, err := engine.NewExecutor(nil, &engine.ExecutorConfig{})
+		require.Error(t, err)
+		require.Nil(t, exe)
+	})
+	t.Run("it returns a new executor", func(t *testing.T) {
+		exe, err := engine.NewExecutor(NewMockContainerExecutor(ctrl), &engine.ExecutorConfig{})
+		require.NoError(t, err)
+		require.NotNil(t, exe)
+	})
+}
+
+func TestExecutor_Execute(t *testing.T) {
+	const (
+		targetComponentName        = "target-component-name"
+		targetComponentStepName    = "clone"
+		targetComponentImage       = "ghcr.io/smithy-security/smithy/components/targets/git-clone:latest"
+		scanner1ComponentName      = "scanner-component-name-1"
+		scanner1ComponentStepName1 = "scanner-1"
+		scanner1ComponentStepName2 = "scanner-3"
+		scanner1ComponentImage     = "ghcr.io/smithy-security/smithy/components/scanners/scanner-1:latest"
+		scanner2ComponentName      = "scanner-component-name-2"
+		scanner2ComponentImage     = "ghcr.io/smithy-security/smithy/components/scanners/scanner-2:latest"
+		scanner2ComponentStepName  = "scanner-2"
+		enricherComponentName      = "enricher-component-name"
+		enricherComponentImage     = "ghcr.io/smithy-security/smithy/components/enrichers/enricher:latest"
+		enricherComponentStepName  = "enricher"
+		filterComponentName        = "filter-component-name"
+		filterComponentImage       = "ghcr.io/smithy-security/smithy/components/filters/filter:latest"
+		filterComponentStepName    = "filter"
+		reporterComponentName      = "reporter-component-name"
+		reporterComponentImage     = "ghcr.io/smithy-security/smithy/components/reporters/reporter:latest"
+		reporterComponentStepName  = "reporter"
+	)
+
+	instanceID, err := uuid.Parse("37087cc2-e4ba-4fe0-8230-cda372778ed7")
+	require.NoError(t, err)
+
+	var (
+		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+		ctrl        = gomock.NewController(t)
+		idGenerator = func() uuid.UUID {
+			return instanceID
+		}
+		mockContainerExec = NewMockContainerExecutor(ctrl)
+		workflow          = &v1.Workflow{
+			Name:        "test-workflow",
+			Description: "test workflow",
+			Stages: []v1.Stage{
+				{
+					ComponentRefs: []v1.ComponentRef{
+						{
+							Component: v1.Component{
+								Name:        targetComponentName,
+								Description: "target-component-descr",
+								Type:        v1.ComponentTypeTarget,
+								Steps: []v1.Step{
+									{
+										Name:  targetComponentStepName,
+										Image: targetComponentImage,
+										EnvVars: map[string]string{
+											"REPO_URL": "github.com/andream16/tree",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					ComponentRefs: []v1.ComponentRef{
+						{
+							Component: v1.Component{
+								Name:        scanner1ComponentName,
+								Description: "scanner-component-descr-1",
+								Type:        v1.ComponentTypeScanner,
+								Steps: []v1.Step{
+									{
+										Name:  scanner1ComponentStepName1,
+										Image: scanner1ComponentImage,
+									},
+									{
+										Name:  scanner1ComponentStepName2,
+										Image: scanner1ComponentImage,
+									},
+								},
+							},
+						},
+						{
+							Component: v1.Component{
+								Name:        scanner2ComponentName,
+								Description: "scanner-component-descr-2",
+								Type:        v1.ComponentTypeScanner,
+								Steps: []v1.Step{
+									{
+										Name:  scanner2ComponentStepName,
+										Image: scanner2ComponentImage,
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					ComponentRefs: []v1.ComponentRef{
+						{
+							Component: v1.Component{
+								Name:        enricherComponentName,
+								Description: "enricher-component",
+								Type:        v1.ComponentTypeEnricher,
+								Steps: []v1.Step{
+									{
+										Name:  enricherComponentStepName,
+										Image: enricherComponentImage,
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					ComponentRefs: []v1.ComponentRef{
+						{
+							Component: v1.Component{
+								Name:        filterComponentName,
+								Description: "filter-component",
+								Type:        v1.ComponentTypeFilter,
+								Steps: []v1.Step{
+									{
+										Name:  filterComponentStepName,
+										Image: filterComponentImage,
+									},
+								},
+							},
+						},
+					},
+				},
+				{
+					ComponentRefs: []v1.ComponentRef{
+						{
+							Component: v1.Component{
+								Name:        reporterComponentName,
+								Description: "reporter-component",
+								Type:        v1.ComponentTypeReporter,
+								Steps: []v1.Step{
+									{
+										Name:       reporterComponentStepName,
+										Image:      reporterComponentImage,
+										Executable: "/bin/reporter",
+										Args:       []string{"-arg1=1"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+	)
+
+	exe, err := engine.NewExecutor(
+		mockContainerExec,
+		&engine.ExecutorConfig{
+			InstanceIDGenerator: idGenerator,
+		},
+	)
+	require.NoError(t, err)
+	require.NotNil(t, exe)
+
+	absPath, err := filepath.Abs(".")
+	require.NoError(t, err)
+
+	var (
+		volumeBindings = []string{
+			path.Join(absPath, ".smithy:/workspace"),
+			fmt.Sprintf("%s:/workspace/repos", os.TempDir()),
+		}
+		envVars = []string{
+			fmt.Sprintf("SMITHY_INSTANCE_ID=%s", instanceID.String()),
+			"SMITHY_LOG_LEVEL=debug",
+		}
+		platform = &ocispec.Platform{
+			Architecture: "amd64",
+			OS:           "linux",
+		}
+	)
+
+	defer cancel()
+
+	t.Run("it executes a workflow correctly", func(t *testing.T) {
+		gomock.InOrder(
+			mockContainerExec.
+				EXPECT().
+				RunAndWait(
+					ctx,
+					engine.ContainerConfig{
+						Name:           targetComponentStepName,
+						Image:          targetComponentImage,
+						Cmd:            make([]string, 0),
+						EnvVars:        append(envVars, "REPO_URL=github.com/andream16/tree"),
+						VolumeBindings: volumeBindings,
+						Platform:       platform,
+					},
+				).
+				Return(nil),
+			mockContainerExec.
+				EXPECT().
+				RunAndWait(
+					ctx,
+					engine.ContainerConfig{
+						Name:           scanner1ComponentStepName1,
+						Image:          scanner1ComponentImage,
+						Cmd:            make([]string, 0),
+						EnvVars:        envVars,
+						VolumeBindings: volumeBindings,
+						Platform:       platform,
+					},
+				).
+				Return(nil),
+			mockContainerExec.
+				EXPECT().
+				RunAndWait(
+					ctx,
+					engine.ContainerConfig{
+						Name:           scanner1ComponentStepName2,
+						Image:          scanner1ComponentImage,
+						Cmd:            make([]string, 0),
+						EnvVars:        envVars,
+						VolumeBindings: volumeBindings,
+						Platform:       platform,
+					},
+				).
+				Return(nil),
+			mockContainerExec.
+				EXPECT().
+				RunAndWait(
+					ctx,
+					engine.ContainerConfig{
+						Name:           scanner2ComponentStepName,
+						Image:          scanner2ComponentImage,
+						Cmd:            make([]string, 0),
+						EnvVars:        envVars,
+						VolumeBindings: volumeBindings,
+						Platform:       platform,
+					},
+				).
+				Return(nil),
+			mockContainerExec.
+				EXPECT().
+				RunAndWait(
+					ctx,
+					engine.ContainerConfig{
+						Name:           enricherComponentStepName,
+						Image:          enricherComponentImage,
+						Cmd:            make([]string, 0),
+						EnvVars:        envVars,
+						VolumeBindings: volumeBindings,
+						Platform:       platform,
+					},
+				).
+				Return(nil),
+			mockContainerExec.
+				EXPECT().
+				RunAndWait(
+					ctx,
+					engine.ContainerConfig{
+						Name:           filterComponentStepName,
+						Image:          filterComponentImage,
+						Cmd:            make([]string, 0),
+						EnvVars:        envVars,
+						VolumeBindings: volumeBindings,
+						Platform:       platform,
+					},
+				).
+				Return(nil),
+			mockContainerExec.
+				EXPECT().
+				RunAndWait(
+					ctx,
+					engine.ContainerConfig{
+						Name:  reporterComponentStepName,
+						Image: reporterComponentImage,
+						Cmd: []string{
+							"/bin/reporter",
+							"-arg1=1",
+						},
+						EnvVars:        envVars,
+						VolumeBindings: volumeBindings,
+						Platform:       platform,
+					},
+				).
+				Return(nil),
+		)
+
+		require.NoError(t, exe.Execute(ctx, workflow))
+	})
+}
