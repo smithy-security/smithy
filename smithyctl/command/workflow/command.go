899e4a211d74ce5876455925d92c33e58b3b7ba6
andream16 <andrea@smithy.security>
2025-02-10 17:34:35 +0000

    Connecting workflow command together

--- smithyctl/command/workflow/command.go
+++ smithyctl/command/workflow/command.go
@@ -7,10 +7,9 @@ func NewCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "workflow",
 		Short: "Allows interacting with workflows",
-		// TODO: implement in https://linear.app/smithy/issue/OCU-476/run-workflows-locally
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
-		},
 	}
+
+	cmd.AddCommand(NewRunCommand())
+
 	return cmd
 }
