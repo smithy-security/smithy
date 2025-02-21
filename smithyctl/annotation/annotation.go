039957813c457101b909aae40251173ca9608857
andream16 <andrea@smithy.security>
2025-02-21 16:54:34 +0000

    Reusable packaged component annotations.

---
+++ smithyctl/annotation/annotation.go
@@ -0,0 +1,12 @@
+package annotation
+
+// Known annotations for component packages.
+const (
+	SmithySDKVersion       = "smithy.sdk.version"
+	SmithyComponentDescr   = "smithy.component.description"
+	SmithyComponentName    = "smithy.component.name"
+	SmithyComponentType    = "smithy.component.type"
+	SmithyComponentVersion = "smithy.component.version"
+	SmithyComponentSource  = "smithy.component.source"
+	SmithyComponentURL     = "smithy.component.url"
+)
