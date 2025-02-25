2ab581b4176f1c7ea88e14de5ffd50422064557c
sg <spyros@ocurity.com>
2025-02-25 18:00:31 +0000

    new codeql dependencies

---
+++ new-components/scanners/codeql/vendor/github.com/smithy-security/smithy/components/component.go
@@ -0,0 +1,10 @@
+package components
+
+const (
+	// EnvSmithyStartTime Start Time of Smithy Scan in RFC3339.
+	EnvSmithyStartTime = "SMITHY_SCAN_TIME"
+	// EnvSmithyScanID the ID of the smithy scan.
+	EnvSmithyScanID = "SMITHY_SCAN_ID"
+	// EnvSmithyScanTags the tags of the smithy scan.
+	EnvSmithyScanTags = "SMITHY_SCAN_TAGS"
+)
