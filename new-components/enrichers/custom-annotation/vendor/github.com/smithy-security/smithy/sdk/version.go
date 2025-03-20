d328bf346a77df6a35cef6f5808f5b3d85449531
andream16 <andrea@smithy.security>
2025-03-20 13:40:39 +0000

    Bumping SDK to v0.0.7-alpha in components.

--- new-components/enrichers/custom-annotation/vendor/github.com/smithy-security/smithy/sdk/version.go
+++ new-components/enrichers/custom-annotation/vendor/github.com/smithy-security/smithy/sdk/version.go
@@ -1,3 +1,5 @@
 package sdk
 
-const Version = "0.0.3-alpha"
+// Version is the SDK's version which is overridable at build time with:
+// go build -ldflags "-X github.com/smithy-security/smithy/sdk.Version=1.2.3"
+var Version = "development"
