5e267d92cbe6a666f49974846fffb9d3aaf75f8e
sg <spyros@ocurity.com>
2025-03-20 20:43:37 +0000

    close OCU-515 image downloader target

---
+++ new-components/targets/image-get/skopeo-getter/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -ex
+
+check_var() {
+  if [ -n "$1" ]; then
+    res="$1"
+    if [ -n "$2" ]; then
+        echo "$res:$2"
+    else
+        echo $res
+    fi
+  else
+    echo "''"
+  fi
+}
+
+
+creds=$(check_var $3 $4)
+skopeo copy \
+            --dest-tls-verify=false\
+            --src-tls-verify=false\
+            --src-creds=$creds \
+            "docker://${1}"\
+            "docker-archive:${2}"
+
+
