3454617c9d1c26bb616edc0befc8781bf1d81275
northdpole <spyros@smithy.security>
2025-04-02 13:49:12 +0100

    remove pip-audit, it's impossible to orchestrate correctly

--- components/scanners/pip-audit/vendor/github.com/smithy-security/smithy/sdk/component/store/remote/postgresql/sqlc/migrations/20241209103854_findings_table.sql
+++
@@ -1,8 +0,0 @@
--- create "finding" table
-CREATE TABLE IF NOT EXISTS finding (
-   id SERIAL PRIMARY KEY,
-   instance_id UUID NOT NULL,
-   details JSONB NOT NULL,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
