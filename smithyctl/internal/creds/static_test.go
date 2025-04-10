81ee1b810ab8e24eb8cc69f4139f4c20aa4050a2
Pavlos Tzianos <pavlos.tzianos@gmail.com>
2025-04-10 17:08:50 +0100

    part of ocu-528: add internal package for managing OCI credentials

---
+++ smithyctl/internal/creds/static_test.go
@@ -0,0 +1,47 @@
+package creds
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestStaticCredsStore(t *testing.T) {
+	testCases := []struct {
+		name, host, username, password string
+		errExpected                    bool
+	}{
+		{
+			name:        "no host causes error",
+			host:        "",
+			username:    "foo",
+			password:    "bar",
+			errExpected: true,
+		},
+		{
+			name:        "no username causes an error",
+			host:        "localhost:5000",
+			username:    "",
+			password:    "bar",
+			errExpected: true,
+		},
+		{
+			name:        "no password casues an error",
+			host:        "localhost:5000",
+			username:    "foo",
+			password:    "",
+			errExpected: true,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			ss, err := NewStaticStore(tt.host, tt.username, tt.password)
+			if tt.errExpected {
+				require.Error(t, err)
+			} else {
+				require.NotNil(t, ss)
+			}
+		})
+	}
+}
