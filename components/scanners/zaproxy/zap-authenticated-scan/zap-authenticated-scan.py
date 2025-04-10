c21358d535c01fdf484bc3815eb5d5fbbe87f1de
Spyros <spyros@smithy.security>
2025-04-10 21:44:46 +0100

    make zap work with browser based auth that is much more recent than form based

--- components/scanners/zaproxy/zap-authenticated-scan/zap-authenticated-scan.py
+++ components/scanners/zaproxy/zap-authenticated-scan/zap-authenticated-scan.py
@@ -32,10 +32,15 @@ class ZapRunner:
     context_name: str = "Default Context"
     zap: ZAPv2
     zap_api_url: str
-    zap_process:None
+    zap_process: None
 
     def __init__(
-        self, api_key: str, target_url: str, host: str = "localhost", port: int = 8090
+        self,
+        api_key: str,
+        target_url: str,
+        host: str = "localhost",
+        port: int = 8090,
+        start_zap: bool = True,
     ):
         parsed = urlparse(target_url)
         if not bool(parsed.scheme and parsed.netloc):
@@ -49,36 +54,44 @@ class ZapRunner:
         self.zap_api_url = f"http://{host}:{port}"
         self.request_proxies = {"http": self.zap_api_url, "https": self.zap_api_url}
         self.zap = ZAPv2(apikey=api_key, proxies=self.request_proxies)
-        
-        self.start_zap()
-        self.wait_for_zap_to_start()
+
+        if start_zap:
+            self.start_zap()
+            self.wait_for_zap_to_start()
         self.check_connection()
         self.create_context()
 
     def wait_for_zap_to_start(self):
-        print('Sleeping 15 seconds to let zap init')
+        print("Sleeping 15 seconds to let zap init")
         time.sleep(15)
-        
+
     def start_zap(self):
         print(f"initializing zap, listening on localhost:8090")
-        self.zap_process = subprocess.Popen(["/zap/zap.sh",
-                                             "-daemon",
-                                             "-silent",
-                                             "-notel",
-                                             "-config", 
-                                            f"api.key={self.api_key}",
-                                             "-host",
-                                             "localhost",
-                                             "-port",
-                                             "8090"],
-                                             stdout=sys.stdout, stderr=sys.stderr, text=True)
+        self.zap_process = subprocess.Popen(
+            [
+                "/zap/zap.sh",
+                "-daemon",
+                "-silent",
+                "-notel",
+                "-config",
+                f"api.key={self.api_key}",
+                "-host",
+                "localhost",
+                "-port",
+                "8090",
+            ],
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            text=True,
+        )
         print(f"initializing zap, subprocess success")
-        
-    def stop_zap(self):
-        print(f"shutting down zap, {self.zap.core.shutdown(apikey=self.api_key)}") # stop zap
-        time.sleep(10)
-        os.killpg(os.getpgid(self.zap_process.pid), signal.SIGTERM) # politely
 
+    def stop_zap(self):
+        print(
+            f"shutting down zap, {self.zap.core.shutdown(apikey=self.api_key)}"
+        )  # stop zap
+        time.sleep(10)
+        os.killpg(os.getpgid(self.zap_process.pid), signal.SIGTERM)  # politely
 
     def check_connection(self):
         version = self.zap.core.version
@@ -86,39 +99,38 @@ class ZapRunner:
         if not version:
             raise RuntimeError(f"could not connect to remote zap at {self.zap_api_url}")
         print(f"connected to remote zap version {version}")
-    
+
     def create_context(self):
         self.context_id = self.zap.context.new_context(self.context_name)
+        if self.context_id == "already_exists":
+            ctx = self.zap.context.context(self.context_name)
+            self.context_id = ctx["id"]
         print(f"context is {self.zap.context.context(self.context_name)}")
 
-    def set_include_in_context(self, target_url: str, logout_url: str = ""):
-        exclude_url = logout_url
+    def set_include_in_context(self, target_url: str):
         include_url = f"{target_url}.*"
-        self.zap.context.include_in_context(self.context_name, include_url)
-        self.excluded_regexp = f"^(?=.*\b{include_url}\b)(?!.*\b{exclude_url}\b).*$"
-        self.zap.context.exclude_from_context(self.context_name, self.excluded_regexp)
         print(
-            f"Configured include regexp {include_url} and exclude regexp {exclude_url} in context"
+            f"Configured include regexp {include_url}, response: {self.zap.context.include_in_context(self.context_name, include_url)}"
         )
 
-    def set_logged_in_indicator(self, logged_in_regexp: str):
-        self.zap.authentication.set_logged_in_indicator(
-            self.context_id, logged_in_regexp
-        )
-        print(f"Configured logged in indicator regex: {logged_in_regexp}")
+    def set_browser_based_auth(self, login_url: str):
 
-    def set_form_based_auth(
-        self, login_url: str, login_request_data_override: str = None
-    ):
-        login_request_data = "username={%username%}&password={%password%}"
-        if login_request_data_override:
-            login_request_data = login_request_data_override
-        form_based_config = f"loginUrl={urllib.parse.quote(login_url)}&loginRequestData={urllib.parse.quote(login_request_data)}"
-        self.zap.authentication.set_authentication_method(
-            self.context_id, "formBasedAuthentication", form_based_config
+        browser_based_config = (
+            f"loginPageUrl={urllib.parse.quote(login_url)}&browserId=firefox-headless"
         )
+        resp = self.zap.authentication.set_authentication_method(
+            contextid=self.context_id,
+            authmethodname="browserBasedAuthentication",
+            authmethodconfigparams=browser_based_config,
+        )
+        
+        if resp != "OK":
+            raise RuntimeError(
+                "could not setup authentication for login_url: {login_url}, response: {resp}"
+            )
+        resp = self.zap.sessionManagement.set_session_management_method(contextid=self.context_id,methodname="autoDetectSessionManagement")
         print(
-            f"Configured form based authentication for login url: {login_url} with request data:{login_request_data}"
+            f"Configured browser based authentication for login url: {login_url}, response: {resp}"
         )
 
     def set_user_auth_config(self, user: str, username: str, password: str):
@@ -138,15 +150,30 @@ class ZapRunner:
         print(f"User Auth Configured for user {user} with username {username}")
         return self.user_id
 
+    def test_user_auth(
+        self,
+    ):
+        print(
+            f"authentication as user:"
+            f" {self.zap.users.authenticate_as_user(userid=self.user_id,contextid=self.context_id)}"
+        )
+
     def start_authenticated_spider(self, user_id):
         scan_id = self.zap.spider.scan_as_user(
-            self.context_id, user_id, self.target_url, recurse="true"
+            contextid=self.context_id,
+            userid=user_id,
+            url=self.target_url,
+            recurse="true",
+            maxchildren=500,
+            subtreeonly=True,
         )
         print(f"Started Spidering with Authentication for zap userid: {user_id}")
         return scan_id
 
     def start_spider(self):
-        scan_id = self.zap.spider.scan(url=self.target_url, recurse=False,subtreeonly=True)
+        scan_id = self.zap.spider.scan(
+            url=self.target_url, recurse=False, subtreeonly=True
+        )
         print(f"Started Spidering")
         return scan_id
 
@@ -155,9 +182,6 @@ class ZapRunner:
 
     def active_authenticated_scan(self, scan_duration):
         self.zap.ascan.set_option_max_scan_duration_in_mins(scan_duration)
-        self.zap.ascan.exclude_from_scan(
-            regex=self.excluded_regexp, apikey=self.api_key
-        )
         return self.zap.ascan.scan_as_user(
             url=self.target_url,
             contextid=self.context_id,
@@ -168,9 +192,6 @@ class ZapRunner:
 
     def active_scan(self, scan_duration):
         self.zap.ascan.set_option_max_scan_duration_in_mins(scan_duration)
-        self.zap.ascan.exclude_from_scan(
-            regex=self.excluded_regexp, apikey=self.api_key
-        )
         return self.zap.ascan.scan(
             url=self.target_url,
             contextid=self.context_id,
@@ -213,18 +234,6 @@ def main():
         default=get_env_or_default("", "LOGIN_URL"),
         type=str,
         help="login url",
-    )
-    parser.add_argument(
-        "--login-indicator",
-        default=get_env_or_default("", "LOGIN_INDICATOR"),
-        type=str,
-        help="login indicator",
-    )
-    parser.add_argument(
-        "--logout-indicator",
-        default=get_env_or_default("", "LOGOUT_INDICATOR"),
-        type=str,
-        help="logout indicator",
     )
     parser.add_argument(
         "--logout-url",
@@ -285,29 +294,33 @@ def main():
     runner = ZapRunner(api_key=args.api_key, target_url=args.target)
     try:
         if args.username and args.password:
-            run_zap_authenticated_scan(args,runner)
+            run_zap_authenticated_scan(args, runner)
         else:
-            run_zap_baseline_scan(args,runner)
+            run_zap_baseline_scan(args, runner)
     finally:
         runner.stop_zap()
 
 
-def run_zap_authenticated_scan(args,runner:ZapRunner):
+def run_zap_authenticated_scan(args, runner: ZapRunner):
     print(
         f"received a username and a password, running an authenticated scan for target: '{args.target}'"
     )
     sub_target_list = args.sub_targets.split(",") if args.sub_targets else []
 
-    runner.set_form_based_auth(login_url=args.login_url)
+
+    runner.set_browser_based_auth(login_url=args.login_url)
     user_id_response = runner.set_user_auth_config(
         user=args.username, username=args.username, password=args.password
     )
-    runner.set_include_in_context(logout_url=args.logout_url, target_url=args.target)
-    runner.set_logged_in_indicator(logged_in_regexp=args.login_indicator)
+    runner.test_user_auth()
+    
+    runner.set_include_in_context(target_url=args.target)
     spider_id = runner.start_authenticated_spider(user_id=user_id_response)
 
     spider_status = runner.get_spider_status(spider_id)
     while "100" not in spider_status:
+        if spider_status == "does_not_exist":
+            raise RuntimeError("spider scan never started")
         print(f"spider scan status: {spider_status}%")
         time.sleep(10)
         spider_status = runner.get_spider_status(spider_id)
@@ -324,7 +337,7 @@ def run_zap_authenticated_scan(args,runner:ZapRunner):
     )
 
 
-def run_zap_baseline_scan(args, runner:ZapRunner):
+def run_zap_baseline_scan(args, runner: ZapRunner):
     print(
         f"did not receive a username and a password, running an unauthenticated baseline scan for target '{args.target}'"
     )
@@ -337,7 +350,7 @@ def run_zap_baseline_scan(args, runner:ZapRunner):
         time.sleep(10)
         spider_status = runner.get_spider_status(spider_id)
     print("finished spidering")
-    
+
     print("running active scan")
     scan_id = runner.active_scan(args.max_scan_duration)
     scan_status = runner.get_scan_status(scanID=scan_id)
@@ -351,6 +364,74 @@ def run_zap_baseline_scan(args, runner:ZapRunner):
     )
 
 
-    
 if __name__ == "__main__":
     main()
+
+# the above script is a much more debuggable and configurable version of the below
+# automation_framework_script=f"""
+# ---
+# # A plan which aims to work out how to configure authentication given the following env vars:
+# #   ZAP_SITE         The target site, e.g. https://www.example.com - must not include the path or a trailing slash
+# #   ZAP_LOGIN_URL    The URL of the login page, e.g. https://www.example.com/login
+# #   ZAP_USER         A valid username
+# #   ZAP_PASSWORD     The associated password
+# #
+# # The report generated will give full details of the session handling and verification details found.
+# # For details see https://www.zaproxy.org/docs/desktop/addons/authentication-helper/auth-report-json/
+# env:
+#   contexts:
+#   - name: Default Context
+#     urls:
+#     - {ZAP_SITE}
+#     includePaths:
+#     - {ZAP_SITE}.*
+#     authentication:
+#       method: browser
+#       parameters:
+#         browserId: firefox-headless
+#         loginPageUrl: {ZAP_LOGIN_URL}
+#         loginPageWait: 5
+#       verification:
+#         method: autodetect
+#     sessionManagement:
+#       method: autodetect
+#     technology: {{}}
+#     users:
+#     - name: test-user
+#       credentials:
+#         username: {ZAP_USER}
+#         password: {ZAP_PASSWORD}
+#   parameters: {{}}
+# jobs:
+# - type: passiveScan-config
+#   parameters:
+#     disableAllRules: true
+#   rules:
+#   - name: Authentication Request Identified
+#     id: 10111
+#     threshold: medium
+#   - name: Session Management Response Identified
+#     id: 10112
+#     threshold: medium
+#   - name: Verification Request Identified
+#     id: 10113
+#     threshold: medium
+# - type: requestor
+#   parameters:
+#     user: test-user
+#   requests:
+#   - url: {ZAP_SITE}
+# - type: passiveScan-wait
+#   parameters: {{}}
+# - name: auth-test-report
+#   type: report
+#   parameters:
+#     template: auth-report-json
+#     theme: null
+#     reportDir: .
+#     reportFile: auth-report.json
+#     reportTitle: ZAP Report
+#   sections:
+#   - summary
+#   - afenv
+#   - statistics"""
