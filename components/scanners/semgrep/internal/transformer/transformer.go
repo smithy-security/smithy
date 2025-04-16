1a4cfa83b0e20223c873aa076cb536fdeef7d3a5
Spyros <spyros@smithy.security>
2025-04-16 12:27:19 +0100

    make semgrep propagate metadata

--- components/scanners/semgrep/internal/transformer/transformer.go
+++ components/scanners/semgrep/internal/transformer/transformer.go
@@ -10,6 +10,8 @@ import (
 	"github.com/smithy-security/pkg/env"
 	sarif "github.com/smithy-security/pkg/sarif"
 	sarifschemav210 "github.com/smithy-security/pkg/sarif/spec/gen/sarif-schema/v2-1-0"
+	ocsffindinginfo "github.com/smithy-security/smithy/sdk/gen/ocsf_ext/finding_info/v1"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	"github.com/smithy-security/smithy/sdk/component"
 	ocsf "github.com/smithy-security/smithy/sdk/gen/ocsf_schema/v1"
@@ -150,5 +152,28 @@ func (g *semgrepTransformer) Transform(ctx context.Context) ([]*ocsf.Vulnerabili
 	if err != nil {
 		return nil, err
 	}
-	return transformer.ToOCSF(ctx)
+	ocsfVulns, err := transformer.ToOCSF(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return g.AddMetadataToDatasources(ctx, ocsfVulns)
+}
+
+func (g *semgrepTransformer) AddMetadataToDatasources(ctx context.Context, findings []*ocsf.VulnerabilityFinding) ([]*ocsf.VulnerabilityFinding, error) {
+	targetMetadata := component.TargetMetadataFromCtx(ctx)
+	for _, f := range findings {
+		for i, source := range f.FindingInfo.DataSources {
+			dataSource := ocsffindinginfo.DataSource{}
+			if err := protojson.Unmarshal([]byte(source), &dataSource); err != nil {
+				return nil, errors.Errorf("could not unmarshal datasource %s, err:%w", source, err)
+			}
+			dataSource.SourceCodeMetadata = targetMetadata.SourceCodeMetadata
+			metadataSource, err := protojson.Marshal(&dataSource)
+			if err != nil {
+				return nil, errors.Errorf("could not marshal new datasource with metdata err:%w", err)
+			}
+			f.FindingInfo.DataSources[i] = string(metadataSource)
+		}
+	}
+	return findings, nil
 }
