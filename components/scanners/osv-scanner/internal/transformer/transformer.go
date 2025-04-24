596ee44c06ff39b860467f00c00f06b6e98e5cc9
Spyros <spyros@smithy.security>
2025-04-24 11:53:17 +0100

    explicitly set the target type for osv-scanner

--- components/scanners/osv-scanner/internal/transformer/transformer.go
+++ components/scanners/osv-scanner/internal/transformer/transformer.go
@@ -160,6 +160,7 @@ func (b *OSVScannerTransformer) AddMetadataToDatasources(ctx context.Context, fi
 			if err := protojson.Unmarshal([]byte(source), &dataSource); err != nil {
 				return nil, errors.Errorf("could not unmarshal datasource %s, err:%w", source, err)
 			}
+			dataSource.TargetType = ocsffindinginfo.DataSource_TARGET_TYPE_REPOSITORY
 			dataSource.SourceCodeMetadata = targetMetadata.SourceCodeMetadata
 			metadataSource, err := protojson.Marshal(&dataSource)
 			if err != nil {
