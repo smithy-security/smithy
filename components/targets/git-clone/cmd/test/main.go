a7b0654c047270cea52159fdcafb42e89cdab116
andream16 <andrea@smithy.security>
2025-05-08 17:28:30 +0100

    Refactoring Clone and adding Diff method to compute diff with base branch dynamically.

---
+++ components/targets/git-clone/cmd/test/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/go-errors/errors"
+
+	"github.com/smithy-security/smithy/new-components/targets/git-clone/internal/target"
+	"github.com/smithy-security/smithy/new-components/targets/git-clone/pkg/git"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+
+	if err := Main(ctx); err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func Main(ctx context.Context) error {
+	conf, err := git.NewConf()
+	if err != nil {
+		return errors.Errorf("could not create new configuration: %w", err)
+	}
+
+	gitManager, err := git.NewManager(conf)
+	if err != nil {
+		return errors.Errorf("could not create new git manager: %w", err)
+	}
+
+	gitCloneTarget, err := target.NewTarget(conf, gitManager)
+	if err != nil {
+		return errors.Errorf("could not create git clone target: %w", err)
+	}
+
+	return gitCloneTarget.Prepare(ctx)
+}
--- components/targets/git-clone/pkg/git/git.go
+++ components/targets/git-clone/pkg/git/git.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log/slog"
 	"net/url"
+	"path"
 	"strings"
 
+	"github.com/bluekeyes/go-gitdiff/gitdiff"
 	"github.com/go-errors/errors"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
@@ -23,12 +25,12 @@ const (
 type (
 	// Repository wraps a git repository to not leak deps.
 	Repository struct {
-		repo *git.Repository
+		Repo    *git.Repository
+		BaseRef string
 	}
 
 	Manager struct {
-		clonePath    string
-		metadataPath string
+		conf         *Conf
 		cloneOptions *git.CloneOptions
 	}
 )
@@ -58,14 +60,9 @@ func NewManager(conf *Conf) (*Manager, error) {
 		// Depth 1 specifies that we want to check out the last commit only.
 		// Makes cloning faster as we don't get the full git history.
 		Depth: 1,
-		// ReferenceName is the reference name.
-		ReferenceName: plumbing.ReferenceName(conf.Reference),
 		// ShallowSubmodules limits cloning submodules to the 1 level of depth.
 		// Makes cloning faster for submodules as we don't fetch their full git history.
 		ShallowSubmodules: true,
-		// SingleBranch lets us fetch only the passed branch.
-		// Makes cloning faster as we don't fetch all the branches.
-		SingleBranch: true,
 		// URL specifies the repository url.
 		URL: u.String(),
 	}
@@ -85,31 +82,141 @@ func NewManager(conf *Conf) (*Manager, error) {
 	}
 
 	return &Manager{
-		clonePath:    conf.ClonePath,
-		metadataPath: conf.TargetMetadataPath,
+		conf:         conf,
 		cloneOptions: opts,
 	}, nil
 }
 
-// Clone clones the configured repository.
+// Clone clones the configured repository from its default references and then checks out in the target reference
+// if it differs from the base.
 func (mgr *Manager) Clone(ctx context.Context) (*Repository, error) {
-	logger := component.
-		LoggerFromContext(ctx).
-		With(
-			slog.String("clone_path", mgr.clonePath),
-		)
+	var (
+		clonePath = mgr.conf.ClonePath
+		logger    = component.
+				LoggerFromContext(ctx).
+				With(
+				slog.String("clone_path", clonePath),
+			)
+	)
 
 	logger.Debug("cloning repository...")
-
-	repo, err := git.PlainCloneContext(ctx, mgr.clonePath, false, mgr.cloneOptions)
+	repo, err := git.PlainCloneContext(ctx, clonePath, false, mgr.cloneOptions)
 	if err != nil && !errors.Is(err, transport.ErrEmptyRemoteRepository) {
-		return nil, errors.Errorf("error cloning repository at '%s': %w", mgr.clonePath, err)
+		return nil, errors.Errorf("error cloning repository at '%s': %w", clonePath, err)
+	}
+	logger.Debug("successfully cloned repository")
+
+	logger.Debug("checking out HEAD...")
+	rh, err := repo.Head()
+	if err != nil {
+		return nil, errors.Errorf("error getting HEAD: %w", err)
 	}
 
-	logger.Debug("successfully cloned repository")
+	wt, err := repo.Worktree()
+	if err != nil {
+		return nil, errors.Errorf("error getting worktree: %w", err)
+	}
+	logger.Debug("successfully checked out HEAD!")
+
+	logger.Debug("checking out references...")
+	var isCheckoutErr bool
+	for _, branch := range []plumbing.ReferenceName{
+		plumbing.ReferenceName(path.Join("refs", "heads", mgr.conf.Reference)),
+		plumbing.ReferenceName(path.Join("refs", "remotes", "origin", mgr.conf.Reference)),
+	} {
+		logger.Debug("checking out reference", slog.String("branch", branch.String()))
+		checkoutErr := wt.Checkout(&git.CheckoutOptions{
+			Branch: branch,
+			Force:  true,
+		})
+		if checkoutErr != nil {
+			logger.Warn("couldn't checkout branch, attempting next if available", slog.String("branch", branch.String()))
+			isCheckoutErr = true
+		} else {
+			isCheckoutErr = false
+		}
+	}
+	if isCheckoutErr {
+		logger.Error("checking out reference failed")
+		return nil, errors.Errorf("couldn't checkout branches for reference: %s", mgr.conf.Reference)
+	}
+	logger.Debug("successfully checked out references!")
+
 	return &Repository{
-		repo: repo,
+		Repo:    repo,
+		BaseRef: rh.Name().String(),
 	}, nil
+}
+
+// GetDiff returns the raw git diff between the target reference and the base reference.
+func (r *Repository) GetDiff() (string, error) {
+	currRef, err := r.Repo.Head()
+	switch {
+	case err != nil:
+		return "", errors.Errorf("could not get HEAD: %w", err)
+	case currRef.Name().String() == r.BaseRef:
+		return "", nil
+	}
+
+	currRefCommit, err := r.Repo.CommitObject(currRef.Hash())
+	if err != nil {
+		return "", errors.Errorf("error getting head commit: %w", err)
+	}
+
+	baseRefObj, err := r.Repo.Reference(plumbing.ReferenceName(r.BaseRef), true)
+	if err != nil {
+		return "", errors.Errorf("error getting base reference '%s': %w", r.BaseRef, err)
+	}
+
+	baseCommit, err := r.Repo.CommitObject(baseRefObj.Hash())
+	if err != nil {
+		return "", errors.Errorf("error getting base commit: %w", err)
+	}
+
+	patch, err := baseCommit.Patch(currRefCommit)
+	if err != nil {
+		return "", errors.Errorf("error generating patch: %w", err)
+	}
+
+	patchStr := patch.String()
+	if patchStr == "" {
+		return "", nil
+	}
+
+	files, _, err := gitdiff.Parse(strings.NewReader(patchStr))
+	if err != nil {
+		return "", errors.Errorf("error parsing patch with gitdiff: %w", err)
+	}
+
+	var sb strings.Builder
+	for _, f := range files {
+		sb.WriteString(fmt.Sprintf("diff --git a/%s b/%s\n", f.OldName, f.NewName))
+		if f.IsBinary {
+			sb.WriteString("Binary files differ\n\n")
+			continue
+		}
+
+		for _, fragment := range f.TextFragments {
+			sb.WriteString(
+				fmt.Sprintf("@@ -%d,%d +%d,%d @@\n",
+					fragment.OldPosition,
+					fragment.OldLines,
+					fragment.NewPosition,
+					fragment.NewLines,
+				),
+			)
+
+			for _, line := range fragment.Lines {
+				sb.WriteString(line.Line)
+				if !strings.HasSuffix(line.Line, "\n") {
+					sb.WriteString("\n")
+				}
+			}
+		}
+		sb.WriteString("\n")
+	}
+
+	return sb.String(), nil
 }
 
 func extractRepoName(path string) (string, error) {
