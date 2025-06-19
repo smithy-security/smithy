d7356e1845a3d0b5d219ee3c2220796ddd877144
saibamo <moritz@smithy.security>
2025-06-19 18:39:14 +0100

    feat: implement the parent class for all db_managers, implement abstract methods, defining how the user can expect to work with them

--- sdk/python/smithy_python/dbmanagers/db_manager.py
+++ sdk/python/smithy_python/dbmanagers/db_manager.py
@@ -1,7 +1,9 @@
-from typing import List
-from smithy_python.helpers.logger import log 
+from typing import List, Optional
+from smithy_python.helpers.logger import log
+from abc import ABC, abstractmethod
+from logging import Logger
 
-class DBManager:
+class DBManager(ABC):
     """
     Base class for database managers.
     This class should be extended by specific database managers to implement
@@ -9,19 +11,60 @@ class DBManager:
     Therefore we are only defining the interface here, without any implementation details.
     """
 
-    def __init__(self):
+    def __init__(self, instance_id:str, logger:Optional[Logger] = None) -> None:
         """
         Initializes the DBManager
+        :param instance_id: The UUID of the instance (aka the Workflow run to be analyzed)
+        :type instance_id: str
+        :param logger: Optional an instance of the Logger class for logging, if not provided, a default logger will be used.
+        :type logger: Optional[Logger]
+        :raises TypeError: If the logger is not an instance of Logger or None.
         """
-        self.log = log
+        
+        self.instance_id = instance_id
+
+        if not logger:
+            self.log = log
+
+        elif isinstance(logger, Logger):
+            self.log = logger
+
+        else:
+            raise TypeError("logger must be an instance of Logger or None.")
 
 
+    @abstractmethod
     def get_findings(self) -> List[any]:
         """
-        Retrieves findings from the database using the provided query.
+        Retrieves **all** findings from the postgres database which are associated to the class variable `instance_id`.
         
-        :param query: The query to retrieve findings.
-        :type query: str
-        :return: A list of findings retrieved from the database.
+        :return: A list of findings retrieved from the database
         """
+
+        raise NotImplementedError("This method should be implemented by subclasses.")
+    
+    @abstractmethod
+    def update_findings(self, findings: List[any]) -> bool:
+        """
+        Updates the findings in the database.
+        
+        :param findings: A list of findings to be updated in the database.
+        :type findings: List[any]
+        :return: True if the update was successful, False otherwise.
+        :rtype: bool
+        """
+        
+        raise NotImplementedError("This method should be implemented by subclasses.")
+    
+    @abstractmethod
+    def create_findings(self, findings: List[any]) -> bool:
+        """
+        Creates new findings in the database.
+        
+        :param findings: A list of findings to be created in the database.
+        :type findings: List[any]
+        :return: True if the creation was successful, False otherwise.
+        :rtype: bool
+        """
+        
         raise NotImplementedError("This method should be implemented by subclasses.")
