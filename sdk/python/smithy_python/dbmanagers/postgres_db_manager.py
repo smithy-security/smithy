818b4b59ea5719a95649779e2f8aa6970cf9d9a1
saibamo <moritz@smithy.security>
2025-06-19 18:39:53 +0100

    feat: implement empty, not fully implemented skeletons for SQLite and PostGres

--- sdk/python/smithy_python/dbmanagers/postgres_db_manager.py
+++ sdk/python/smithy_python/dbmanagers/postgres_db_manager.py
@@ -1,28 +1,33 @@
 from typing import List, override, Union, Optional
+from logging import Logger
+from smithy_python.dbmanagers.db_manager import DBManager
 
-
-class PostgresDBManager:
+class PostgresDBManager(DBManager):
     """
     A class to manage PostgreSQL database connections and operations.
     """
 
-    def __init__(self):
+    def __init__(self, instance_id: str, logger: Optional[Logger] = None) -> None:
         """
         Initializes the PostgreSQL database manager with the given configuration.
 
-        :param db_config: Dictionary containing database configuration parameters.
+        :param instance_id: The UUID of the instance (aka the Workflow run to be analyzed)
+        :type instance_id: str
+        :param logger: An instance of the Logger class for logging. If not provided, a default logger will be used.
+        :type logger: Optional[Logger]
+        :raises TypeError: If the logger is not an instance of Logger or None.
         """
-        super().__init__()
+
+        super().__init__(instance_id, logger)
+
         raise NotImplementedError("PostgresDBManager is not yet implemented.")
 
 
     @override
-    def get_findings(self, query: str) -> List[any]:
+    def get_findings(self) -> List[any]:
         """
-        Retrieves findings from the postgres database using the provided query.
-        
-        :param query: The query to retrieve findings.
-        :type query: str
+        Retrieves **all** findings from the postgres database which are associated to the class variable `instance_id`.
+    
         :return: A list of findings retrieved from the postgres database.
         """
 
--- sdk/python/smithy_python/dbmanagers/sqlite_db_manager.py
+++ sdk/python/smithy_python/dbmanagers/sqlite_db_manager.py
@@ -1,6 +1,7 @@
 from .db_manager import DBManager
 import os
-from typing import List, override
+from typing import List, override, Optional
+from logging import Logger
 
 
 class SqliteDBManager(DBManager):
@@ -9,23 +10,26 @@ class SqliteDBManager(DBManager):
     It extends the DBManager class and implements the required methods for managing sqlite database connections.
     """
 
-    def __init__(self):
+    def __init__(self, instance_id: str, logger: Optional[Logger] = None) -> None:
         """
         Initializes the SqliteDBManager
+        :param instance_id: The UUID of the instance (aka the Workflow run to be analyzed)
+        :type instance_id: str
+        :param logger: An instance of the Logger class for logging. If not provided, a default logger will be used.
+        :type logger: Optional[Logger]
+        :raises TypeError: If the logger is not an instance of Logger or None.
         """
         
-        super().__init__()
+        super().__init__(instance_id, logger)
         
         raise NotImplementedError("SQLiteDBManager is not yet implemented.")
         
         
     @override
-    def get_findings(self, query: str) -> List[any]:
+    def get_findings(self) -> List[any]:
         """
-        Retrieves findings from the sqlite database using the provided query.
-        
-        :param query: The query to retrieve findings.
-        :type query: str
+        Retrieves **all** findings from the sqlite database which are associated to the class variable `instance_id`.
+    
         :return: A list of findings retrieved from the sqlite database.
         """
 
