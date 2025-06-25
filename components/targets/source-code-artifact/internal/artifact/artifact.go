c711e66428792e1e9911987a94c09e23c2809cb2
andream16 <andrea@smithy.security>
2025-06-25 15:11:07 +0100

    Entities and utilities to deal with artifact types and remote types.

---
+++ components/targets/source-code-artifact/internal/artifact/artifact.go
@@ -0,0 +1,62 @@
+package artifact
+
+import (
+	"strings"
+
+	"github.com/go-errors/errors"
+)
+
+const (
+	SourceTypeS3          SourceType = "s3"
+	SourceTypeGCS         SourceType = "gcs"
+	SourceTypeRemote      SourceType = "remote"
+	SourceTypeUnsupported SourceType = "unsupported"
+
+	FileTypeZip         FileType = "zip"
+	FileTypeTar         FileType = "tar"
+	FileTypeTarGz       FileType = "tar.gz"
+	FileTypeUnsupported FileType = "unsupported"
+)
+
+type (
+	// SourceType type alias for source types.
+	SourceType string
+	// FileType type alias for file types.
+	FileType string
+)
+
+func (st SourceType) String() string {
+	return string(st)
+}
+
+func (ft FileType) String() string {
+	return string(ft)
+}
+
+// GetSourceType returns the correct source type based on the input one.
+func GetSourceType(source string) (SourceType, error) {
+	switch {
+	case strings.HasPrefix(source, "s3"):
+		return SourceTypeS3, nil
+	case strings.HasPrefix(source, "gs"):
+		return SourceTypeGCS, nil
+	case strings.HasPrefix(source, "http"):
+		return SourceTypeRemote, nil
+	default:
+		return SourceTypeUnsupported, errors.Errorf("unsupported source type: %s", source)
+	}
+}
+
+// GetFileType returns the correct file type based on the input one.
+func GetFileType(fileName string) (FileType, error) {
+	switch {
+	case strings.HasSuffix(fileName, ".tar"):
+		return FileTypeTar, nil
+	case strings.HasSuffix(fileName, ".tar.gz"):
+		return FileTypeTarGz, nil
+	case strings.HasSuffix(fileName, ".zip"):
+		return FileTypeZip, nil
+	}
+
+	return FileTypeUnsupported, errors.Errorf("unsupported file type: %s", fileName)
+}
