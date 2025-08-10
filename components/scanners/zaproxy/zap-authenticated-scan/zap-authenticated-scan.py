6dc7ab3a4c7a6d124293a31138d3a075b738b6cf
Spyros <spyros@smithy.security>
2025-08-10 23:33:46 +0100

    transition the zap orchestrator to use ONLY the automation framework for scanning instead of the hybrid, api and automation framework

--- components/scanners/zaproxy/zap-authenticated-scan/zap-authenticated-scan.py
+++ components/scanners/zaproxy/zap-authenticated-scan/zap-authenticated-scan.py
@@ -3,6 +3,7 @@ import json
 import yaml
 import sys
 import signal
+import re
 import subprocess
 import argparse
 import os
@@ -74,7 +75,8 @@ plan_template = """
             "parameters": {
                 "maxDuration": 5,
                 "maxCrawlDepth": 10,
-                "runOnlyIfModern": true
+                "runOnlyIfModern": true,
+                "inScopeOnly": true
             },
             "name": "spiderAjax",
             "type": "spiderAjax"
@@ -114,6 +116,85 @@ plan_template = """
             }
         }
     ]
+}
+"""
+
+baseline_plan_template = """
+{
+  "env": {
+    "contexts": [
+      {
+        "name": "Smithy Baseline Context",
+        "urls": [
+          "${zap_target}"
+        ],
+        "includePaths": [],
+        "excludePaths": []
+      }
+    ],
+    "parameters": {
+      "failOnError": true,
+      "failOnWarning": false,
+      "progressToStdout": true
+    },
+    "vars": {}
+  },
+  "jobs": [
+    {
+      "parameters": {
+        "scanOnlyInScope": true,
+        "enableTags": false
+      },
+      "rules": [],
+      "name": "passiveScan-config",
+      "type": "passiveScan-config"
+    },
+    {
+      "parameters": {
+        "maxDuration": "${zap_spider_duration_mins}"
+      },
+      "name": "spider",
+      "type": "spider"
+    },
+    {
+      "parameters": {},
+      "name": "passiveScan-wait-pre-ajax",
+      "type": "passiveScan-wait"
+    },
+    {
+      "parameters": {
+        "maxDuration": "${zap_spider_duration_mins}",
+        "runOnlyIfModern": true,
+        "inScopeOnly": true
+      },
+      "name": "spiderAjax",
+      "type": "spiderAjax"
+    },
+    {
+      "parameters": {
+        "maxScanDurationInMins": "${zap_active_scan_duration_mins}"
+      },
+      "policyDefinition": {},
+      "name": "activeScan",
+      "type": "activeScan"
+    },
+    {
+      "parameters": {},
+      "name": "passiveScan-wait-pre-report",
+      "type": "passiveScan-wait"
+    },
+    {
+            "name": "${zap_report_name}",
+            "type": "report",
+            "parameters": {
+                "template": "sarif-json",
+                "theme": null,
+                "reportDir": "${zap_report_dir}",
+                "reportFile": "${zap_report_file}",
+                "reportTitle": "${zap_report_title}"
+            }
+        }
+  ]
 }
 """
 
@@ -238,115 +319,6 @@ class ZapRunner:
             self.context_id = ctx["id"]
         print(f"context is {self.zap.context.context(self.context_name)}")
 
-    def set_include_in_context(self: "ZapRunner", target_url: str) -> None:
-        urlparsed = urlparse(target_url)
-        without_scheme = f"{urlparsed.netloc}{urlparsed.path.rstrip('/')}"
-        include_url = f".*{without_scheme}.*"
-        print(
-            f"Configured include regexp {include_url}, response: {self.zap.context.include_in_context(self.context_name, include_url)}"
-        )
-
-    def set_browser_based_auth(self: "ZapRunner", login_url: str) -> None:
-        browser_based_config = (
-            f"loginPageUrl={quote(login_url)}&browserId=firefox-headless"
-        )
-        resp = self.zap.authentication.set_authentication_method(
-            contextid=self.context_id,
-            authmethodname="browserBasedAuthentication",
-            authmethodconfigparams=browser_based_config,
-        )
-
-        if resp != "OK":
-            raise RuntimeError(
-                "could not setup authentication for login_url: {login_url}, response: {resp}"
-            )
-        resp = self.zap.sessionManagement.set_session_management_method(
-            contextid=self.context_id, methodname="autoDetectSessionManagement"
-        )
-        print(
-            f"Configured browser based authentication for login     url: {login_url}, response: {resp}"
-        )
-
-    def set_user_auth_config(
-        self: "ZapRunner",
-        user: str,
-        username: str,
-        password: str,
-    ) -> None:
-        if not user or not username or not password:
-            raise ValueError(
-                f"parameters 'user', 'username' and 'password' must be set, received: user:{user},username:{username},password:{password}"
-            )
-
-        self.user_id = self.zap.users.new_user(self.context_id, user)
-        user_auth_config = f"username={quote(username)}&password={quote(password)}"
-        self.zap.users.set_authentication_credentials(
-            self.context_id, self.user_id, user_auth_config
-        )
-        self.zap.users.set_user_enabled(self.context_id, self.user_id, "true")
-        self.zap.authentication.get_authentication_method
-        print(f"User Auth Configured for user {user} with username {username}")
-        return self.user_id
-
-    def test_user_auth(
-        self: "ZapRunner",
-    ) -> None:
-        print(
-            f"authentication as user:"
-            f" {self.zap.users.authenticate_as_user(userid=self.user_id,contextid=self.context_id)}"
-        )
-
-    def start_authenticated_spider(self: "ZapRunner", user_id):
-        scan_id = self.zap.spider.scan_as_user(
-            contextid=self.context_id,
-            userid=user_id,
-            url=self.target_url,
-            recurse="true",
-            maxchildren=500,
-            subtreeonly=True,
-        )
-        print(f"Started Spidering with Authentication for zap userid: {user_id}")
-        return scan_id
-
-    def start_spider(self: "ZapRunner"):
-        scan_id = self.zap.spider.scan(
-            url=self.target_url, recurse=False, subtreeonly=True
-        )
-        print(f"Started Spidering")
-        return scan_id
-
-    def get_spider_status(self: "ZapRunner", scanID):
-        return self.zap.spider.status(scanid=scanID)
-
-    def active_authenticated_scan(self: "ZapRunner", scan_duration):
-        self.zap.ascan.set_option_max_scan_duration_in_mins(scan_duration)
-        return self.zap.ascan.scan_as_user(
-            url=self.target_url,
-            contextid=self.context_id,
-            userid=self.user_id,
-            recurse=False,
-            apikey=self.api_key,
-        )
-
-    def active_scan(self: "ZapRunner", scan_duration: int = 0):
-        print(f"setting scan max duration to {scan_duration} minutes")
-        self.zap.ascan.set_option_max_scan_duration_in_mins(scan_duration)
-        return self.zap.ascan.scan(
-            url=self.target_url,
-            contextid=self.context_id,
-            recurse=False,
-            apikey=self.api_key,
-        )
-
-    def get_active_scan_status(self: "ZapRunner", scanID):
-        return self.zap.ascan.status(scanid=scanID)
-
-    def get_passive_scan_status(self: "ZapRunner"):
-        """Get the status of a passive scan.
-        It returns -1 when there are no more record to scan so it is finished.
-        """
-        return self.zap.pscan.records_to_scan
-
     def run_automation_framework_scan(
         self: "ZapRunner",
         login_url: str = None,
@@ -357,20 +329,49 @@ class ZapRunner:
         report_title: str = "Smithy Zap Report",
         report_dir: str = None,
         plan: str = None,
+        scan_duration: int = 10,
+        spider_duration: int = 5,
     ):
         automation_framework_script = json.loads(plan)
         for context in automation_framework_script["env"]["contexts"]:
             print(f"setting zap target for context {context['name']}")
             context["urls"] = [self.target_url]
-            context["includePaths"] = [f"{self.target_url}.*"]
-            context["authentication"]["parameters"]["loginPageUrl"] = login_url
-            for user in context["users"]:
-                user["credentials"]["username"] = username
-                user["credentials"]["password"] = password
+            urlparsed = urlparse(self.target_url)
+            without_scheme = re.escape(
+                f"{urlparsed.netloc}{urlparsed.path.rstrip('/')}"
+            )
+            include_url = f".*{without_scheme}.*"
+
+            context["includePaths"] = [f"{include_url}"]
+            print(f"Configured include paths {context['includePaths']}")
+            context["excludePaths"] = [f"^((?!{without_scheme}).)*$"]
+            print(f"Configured exclude paths {context['excludePaths']}")
+
+            if login_url and username and password:
+                print(
+                    f"setting authentication for context {context['name']} to login_url: {login_url}, username: {username}"
+                )
+                context["authentication"]["method"] = "browser"
+                context["authentication"]["parameters"]["loginPageUrl"] = login_url
+                for user in context["users"]:
+                    user["credentials"]["username"] = username
+                    user["credentials"]["password"] = password
         for job in automation_framework_script["jobs"]:
             if job["type"] == "requestor":
                 for req in job["requests"]:
                     req["url"] = self.target_url
+            elif job["type"] == "activeScan":
+                if "parameters" not in job:
+                    job["parameters"] = {}
+                job["parameters"]["maxScanDurationInMins"] = scan_duration
+            elif job["type"] == "spider":
+                if "parameters" not in job:
+                    job["parameters"] = {}
+                job["parameters"]["maxDuration"] = spider_duration
+            elif job["type"] == "spiderAjax":
+                if "parameters" not in job:
+                    job["parameters"] = {}
+                job["parameters"]["maxDuration"] = spider_duration
             elif job["type"] == "report":
                 job["name"] = report_name
                 job["parameters"]["reportDir"] = report_dir
@@ -394,26 +395,6 @@ class ZapRunner:
             print(f"plan completed successfully at {progress['finished']}")
         if progress["error"]:
             print(f"plan had errors, check progress for hints")
-
-    def get_report(
-        self: "ZapRunner",
-        filename: str = "zap-report.json",
-        title: str = "Smithy ZAP report",
-        report_dir: str = None,
-    ):
-        print(f"generating report")
-        response = requests.get(
-            f"{self.zap.base}reports/action/generate/?apikey={self.api_key}&title={title}&template=sarif-json&theme=&description=&contexts=&sites=&sections=&includedConfidences=&includedRisks=&reportFileName={filename}&reportFileNamePattern=&reportDir={report_dir}&display=",
-            proxies=self.request_proxies,
-        )
-        if response.status_code != 200:
-            from pprint import pprint
-
-            pprint(response.text)
-            raise RuntimeError(
-                f"the zap server replied with {response.status_code} while generating report"
-            )
-        return response.json()["generate"]
 
 
 def get_env_or_default(value, env_key, default=""):
@@ -483,9 +464,10 @@ def main():
         help="where to put the report",
     )
     parser.add_argument(
-        "--use-automation-framework",
-        action="store_true",
-        help="use automation framework to execute the scan",
+        "--max-spider-duration",
+        default=get_env_or_default("", "SPIDER_DURATION_MINS"),
+        type=str,
+        help="where to put the report",
     )
     parser.add_argument(
         "--no-start-zap",
@@ -517,106 +499,41 @@ def main():
         )
 
     runner.create_context()
+    max_scan_duration = 10  # default
+    if args.max_scan_duration and str(args.max_scan_duration).isdecimal():
+        max_scan_duration = int(args.max_scan_duration)
+    else:
+        print(
+            f"max scan duration had unsupported value '{args.max_scan_duration}' setting to default '{max_scan_duration}'"
+        )
+
+    max_spider_duration = 5  # default
+    if args.max_spider_duration and str(args.max_spider_duration).isdecimal():
+        max_spider_duration = int(args.max_spider_duration)
+    else:
+        print(
+            f"max spider duration had unsupported value '{args.max_spider_duration}' setting to default '{max_spider_duration}'"
+        )
 
     try:
         if args.username and args.password:
-            if args.use_automation_framework:
-                print("running automation framework scan")
-                return runner.run_automation_framework_scan(
-                    login_url=args.login_url,
-                    username=args.username,
-                    password=args.password,
-                    report_dir=args.report_dir,
-                    plan=plan_template,
-                )
-            print("running scan using the rest api")
-            return run_zap_authenticated_scan(args, runner)
-
-        print("running baseline scan using the rest api")
-        return run_zap_baseline_scan(args, runner)
+            print("running automation framework scan")
+            return runner.run_automation_framework_scan(
+                login_url=args.login_url,
+                username=args.username,
+                password=args.password,
+                report_dir=args.report_dir,
+                plan=plan_template,
+            )
+        print("running baseline scan using the the automation framework")
+        return runner.run_automation_framework_scan(
+            report_dir=args.report_dir,
+            plan=baseline_plan_template,
+            scan_duration=max_scan_duration,
+            spider_duration=max_spider_duration,
+        )
     finally:
         runner.stop_zap()
-
-
-def run_zap_authenticated_scan(args, runner: ZapRunner):
-    print(
-        f"received a username and a password, running an authenticated scan for target: '{args.target}'"
-    )
-
-    runner.set_browser_based_auth(login_url=args.login_url)
-    user_id_response = runner.set_user_auth_config(
-        user=args.username, username=args.username, password=args.password
-    )
-    runner.test_user_auth()
-
-    runner.set_include_in_context(target_url=args.target)
-    spider_id = runner.start_authenticated_spider(user_id=user_id_response)
-
-    spider_status = runner.get_spider_status(spider_id)
-    while "100" not in spider_status:
-        if spider_status == "does_not_exist":
-            raise RuntimeError("spider scan never started")
-        print(f"spider scan status: {spider_status}%")
-        time.sleep(10)
-        spider_status = runner.get_spider_status(spider_id)
-
-    scan_id = runner.active_authenticated_scan(args.max_scan_duration)
-    scan_status = runner.get_active_scan_status(scanID=scan_id)
-    while "100" not in scan_status:
-        print(f"active scan status: {scan_status}%")
-        time.sleep(10)
-        scan_status = runner.get_active_scan_status(scanID=scan_id)
-
-    print(
-        f"report stored successfully at: {runner.get_report(report_dir=args.report_dir,filename=args.report_name)}"
-    )
-
-
-def run_zap_baseline_scan(args, runner: ZapRunner):
-    print(
-        f"did not receive a username and a password, running an unauthenticated baseline scan for target '{runner.target_url}'"
-    )
-    runner.set_include_in_context(target_url=runner.target_url)
-    spider_id = runner.start_spider()
-    spider_status = runner.get_spider_status(spider_id)
-    while "100" not in spider_status:
-        print(f"spider scan status: {spider_status}%")
-        time.sleep(10)
-        spider_status = runner.get_spider_status(spider_id)
-    print("finished spidering")
-
-    print("running active scan")
-    scan_id = runner.active_scan(args.max_scan_duration)
-    if scan_id == "url_not_in_context":
-        raise RuntimeError("scan url is not in context")
-
-    print(f"started scan with id {scan_id}")
-    scan_status = runner.get_active_scan_status(scanID=scan_id)
-    while "100" not in scan_status:
-        if "DOES_NOT_EXIST" in scan_status:
-            raise RuntimeError("scan was never started")
-        print(f"active scan status: {scan_status}%")
-        time.sleep(10)
-        scan_status = runner.get_active_scan_status(scanID=scan_id)
-    print("finished active scan")
-
-    print("waiting for passive scan to finish")
-    scan_status = runner.get_passive_scan_status()
-    try:
-        while int(scan_status) >= 1:
-            if "DOES_NOT_EXIST" in scan_status:
-                raise RuntimeError("scan was never started")
-            print(f"records left for passive scan to finish: {scan_status}")
-            time.sleep(10)
-            scan_status = runner.get_passive_scan_status()
-    except ValueError as ve:
-        print(f"error while checking passive scan status: {ve}, continuing")
-
-    print(f"finished passive scan, records left: {scan_status}")
-
-    print(
-        f"report stored successfully at: {runner.get_report(report_dir=args.report_dir,filename=args.report_name)}"
-    )
 
 
 if __name__ == "__main__":
