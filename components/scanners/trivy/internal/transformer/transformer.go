019b8337cd003d2714ae79b7f2c67e94745a0734
Spyros <spyros@smithy.security>
2025-08-20 05:16:22 +0100

    make the trivy parser not error out when receiving an empty file

--- components/scanners/trivy/internal/transformer/transformer.go
+++ components/scanners/trivy/internal/transformer/transformer.go
@@ -127,6 +127,11 @@ func (g *trivyTransformer) Transform(ctx context.Context) ([]*ocsf.Vulnerability
 		return nil, errors.Errorf("failed to read raw output file '%s': %w", g.rawOutFilePath, err)
 	}
 
+	if len(b) == 0 {
+		logger.Info("input file is empty, exiting without findings")
+		return []*ocsf.VulnerabilityFinding{}, nil
+	}
+
 	var report sarifschemav210.SchemaJson
 	if err := report.UnmarshalJSON(b); err != nil {
 		return nil, errors.Errorf("failed to parse raw trivy output: %w", err)
