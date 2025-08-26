ed93edccee2136b6a8ee94428802569d465adb2d
senglezou <senglezou@users.noreply.github.com>
2025-08-26 16:02:27 +0100

    Force push comment to QA

--- hack/main.go
+++ hack/main.go
@@ -11,6 +11,8 @@ import (
 
 var db *sql.DB
 
+// FORCE TEST
+
 // VULNERABLE: Direct string concatenation in SQL query
 func getUserByID(userID string) (*User, error) {
 	// This is vulnerable to SQL injection!
