0ea4fa3a8bd06a2abe4d313f5f2f227b2bdde1e1
senglezou <senglezou@users.noreply.github.com>
2025-09-22 16:22:46 +0100

    Add test data

--- components/scanners/sobelow/internal/transformer/transformer_test.go
+++ components/scanners/sobelow/internal/transformer/transformer_test.go
@@ -2,13 +2,18 @@ package transformer_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/jonboulle/clockwork"
+	"github.com/smithy-security/pkg/utils"
 	"github.com/smithy-security/smithy/sdk/component"
 	ocsffindinginfo "github.com/smithy-security/smithy/sdk/gen/ocsf_ext/finding_info/v1"
+	ocsf "github.com/smithy-security/smithy/sdk/gen/ocsf_schema/v1"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/smithy-security/smithy/components/scanners/sobelow/internal/transformer"
 )
@@ -17,8 +22,8 @@ func TestSobelowTransformer_Transform(t *testing.T) {
 	var (
 		ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 		clock       = clockwork.NewFakeClockAt(time.Date(2024, 11, 1, 0, 0, 0, 0, time.UTC))
-		// nowUnix     = clock.Now().Unix()
-		// falseBool   = false
+		nowUnix     = clock.Now().Unix()
+		falseBool   = false
 	)
 	defer cancel()
 
@@ -36,6 +41,53 @@ func TestSobelowTransformer_Transform(t *testing.T) {
 
 	t.Run("it should transform correctly the finding to ocsf format", func(t *testing.T) {
 
+		expectedRelativePath := "lib/carafe_web/controllers/potion_controller.ex"
+
+		expectedDataSource := &ocsffindinginfo.DataSource{
+			TargetType: ocsffindinginfo.DataSource_TARGET_TYPE_REPOSITORY,
+			Uri: &ocsffindinginfo.DataSource_URI{
+				UriSchema: ocsffindinginfo.DataSource_URI_SCHEMA_FILE,
+				Path:      fmt.Sprint("file://" + expectedRelativePath),
+			},
+			SourceCodeMetadata: targetMetadata.SourceCodeMetadata,
+		}
+
+		expectedDataSourceJSON, err := protojson.Marshal(expectedDataSource)
+		require.NoError(t, err)
+
+		expectedFinding := &ocsf.VulnerabilityFinding{
+			FindingInfo: &ocsf.FindingInfo{
+				DataSources: []string{string(expectedDataSourceJSON)},
+			},
+			Vulnerabilities: []*ocsf.Vulnerability{
+				{
+					Desc:          utils.Ptr("blah blah."),
+					Title:         utils.Ptr("blah blah."),
+					Severity:      utils.Ptr(ocsf.VulnerabilityFinding_SEVERITY_ID_HIGH.String()),
+					FirstSeenTime: &nowUnix,
+					LastSeenTime:  &nowUnix,
+					Cwe:           nil,
+					Cve: &ocsf.Cve{
+						Uid:  "blah blah.",
+						Desc: utils.Ptr("blah blah."),
+					},
+					AffectedCode: []*ocsf.AffectedCode{
+						{
+							File: &ocsf.File{
+								Name: "blah blah.",
+								Path: utils.Ptr("blah blah."),
+							},
+						},
+					},
+					VendorName:      utils.Ptr("osv-scanner"),
+					FirstSeenTimeDt: &timestamppb.Timestamp{Seconds: nowUnix},
+					LastSeenTimeDt:  &timestamppb.Timestamp{Seconds: nowUnix},
+					IsFixAvailable:  &falseBool,
+					FixAvailable:    &falseBool,
+				},
+			},
+		}
+
 		ocsfTransformer, err := transformer.New(
 			transformer.SobelowTransformerWithClock(clock),
 		)
@@ -46,6 +98,9 @@ func TestSobelowTransformer_Transform(t *testing.T) {
 		require.NotEmpty(t, findings)
 		require.Len(t, findings, 1)
 
+		actualFinding := findings[0]
+		require.JSONEq(t, string(expectedDataSourceJSON), actualFinding.FindingInfo.DataSources[0])
+		require.Equal(t, expectedFinding.Vulnerabilities, actualFinding.Vulnerabilities)
 	})
 
 }
